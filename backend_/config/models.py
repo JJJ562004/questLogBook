5a880743329e8172571d88c697d3e695b89b8779
JJJ4065 <darknessrisesgamer999@gmail.com>
2025-11-06 10:26:59 +0700

    updated

---
+++ backend_/config/models.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, Field
+from typing import Optional, List
+
+class Student(BaseModel):
+    name: str = Field(..., title="Name of the student", max_length=100)
+    age: int = Field(..., title="Age of the student", ge=0)
+    grade: str = Field(..., title="Grade of the student", max_length=10)
+
+class StudentIn(Student):
+    """Base model for creating a new student."""
+    pass
+
+class StudentOut(Student):
+    """Base model for returning student data."""
+    _id: Optional[str] = Field(default=None, alias="_id")
+
+class StudentCollection(BaseModel):
+    students: List[StudentOut] = Field(..., title="List of students")
